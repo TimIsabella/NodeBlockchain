9674aa0be154a7fb223128f1ff2ad40918df7a5a
TimIsabella <timjisabella@gmail.com>
2022-07-01 15:18:35 -0700

    Block

---
+++ block.js
@@ -0,0 +1,21 @@
+class Block 
+	{
+		//Define attributes of the 'Block' class
+		constructor(timestamp, lastHash, hash, data)
+		{
+		 this.timestamp = timestamp;
+		 this.lastHash = lastHash;
+		 this.hash = hash;
+		 this.data = data;
+		}
+		
+		toString()
+		{
+		 return `Block - \n Timestamp: ${this.timestamp},\n Last Hash: ${this.lastHash},\n Hash: ${this.hash},\n Data: ${this.data}`;
+		 
+		 //Return a shortened version by using '.substring()'
+		 //return `Block - \n Timestamp: ${this.timestamp},\n Last Hash: ${this.lastHash.substring(0, 10)},\n Hash: ${this.hash.substring(0, 10)},\n Data: ${this.data}`;
+		}
+	}
+	
+	module.exports = Block;
