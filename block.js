34497f4f61a9b18b125361b9ccaf15b8052eea9d
TimIsabella <timjisabella@gmail.com>
2022-07-01 15:26:54 -0700

    Genesis block

--- block.js
+++ block.js
@@ -11,10 +11,17 @@ class Block
 		
 		toString()
 		{
-		 return `Block - \n Timestamp: ${this.timestamp},\n Last Hash: ${this.lastHash},\n Hash: ${this.hash},\n Data: ${this.data}`;
+		 return `Block - \n Timestamp: ${this.timestamp}\n Last Hash: ${this.lastHash}\n Hash: ${this.hash}\n Data: ${this.data}`;
 		 
 		 //Return a shortened version by using '.substring()'
 		 //return `Block - \n Timestamp: ${this.timestamp},\n Last Hash: ${this.lastHash.substring(0, 10)},\n Hash: ${this.hash.substring(0, 10)},\n Data: ${this.data}`;
+		}
+		
+		//Very first block in chain
+		//'static' - method that allows calling without having to make a new instance of the block class
+		static genesis()
+		{
+		 return new this('Genesis time', 'Genesis lastHash', 'Genesis hash', 'Genesis data');
 		}
 	}
 	
