b457134ca546dff88c3363f328f5fe80bd2098d3
TimIsabella <timjisabella@gmail.com>
2022-07-21 14:39:28 -0700

    Grab valid transactions

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -1,9 +1,11 @@
+const Transaction = require('../wallet/transaction');
+
 class TransactionPool
 	{
 	 constructor()
 	    {
 		 this.transactions = [];
-		}
+		};
 	 
 	 //Method for adding and updating pool transactions
 	 updateOrAddTransaction(transaction)
@@ -23,14 +25,45 @@ class TransactionPool
 	        //Push 'transaction' parameter to 'transactions' array
 	        this.transactions.push(transaction);
 	       }
-	    }
+	    };
 	
 	 //Verify that a transaction in the pool exists by 'address' wallet public key
      existingTransaction(address)
         {
          //Find transaction within the pool which matches the parameter 'address' wallet public key and return
          return this.transactions.find(t => t.input.address === address);
-        }
+        };
+     
+     //Returns validated transactions
+     validTransactions()
+        {
+         //Check each transaction in the pool and only return if passed validation steps
+         return this.transactions.filter(transaction => {
+                                                         //Add up transaction output amounts to get total
+                                                         const outputTotal = transaction.outputs.reduce((total, output) => {
+                                                                                                                             return total + output.amount;
+                                                                                                                            }, 0 //initial total value
+                                                                                                       );
+                                                         
+                                                         //Compare added transaction ouput total with input amount, and reject if they do not match
+                                                         if(transaction.input.amount !== outputTotal)
+                                                           {
+                                                            console.log(`Invalid transaction from ${transaction.input.address}.`);
+                                                            return;
+                                                           }
+                                                        
+                                                         //Send transaction to 'verifyTransaction' for signature verification, and reject if return false
+                                                         if(!Transaction.verifyTransaction(transaction))
+                                                           {
+                                                            console.log(`Invalid signature from ${transaction.input.address}.`);
+                                                            return;
+                                                           }
+                                                        
+                                                         //Passed all validations
+                                                         return transaction;
+                                                        }
+                                        );
+        };
 	}
 
 module.exports = TransactionPool;
