e6f0739a16647107b26c958e5233407e0c0c0099
TimIsabella <timjisabella@gmail.com>
2022-07-23 13:10:40 -0700

    Test reward transactions

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -1,5 +1,6 @@
 const Transaction = require('./transaction');
 const Wallet = require('./index');
+const { MINING_REWARD } = require('../config');
 
 describe('Transaction', () => {
 							   let transaction, wallet, recipient, amount;
@@ -87,7 +88,21 @@ describe('Transaction', () => {
 							                                                   )
 							                                                }
 							           );
+							          
+							   //
+							   describe('creating a reward transaction', () => {
+							                                                    beforeEach(() => {
+							                                                                      transaction = Transaction.rewardTransaction(wallet, Wallet.blockchainWallet());
+							                                                                     }
+							                                                              );
+							                                                              
+							                                                    it(`reward the miner's wallet`, () => {
+							                                                                                           //Check that amount of 'transaction' matching the wallet is the same as the 'MINING REWARD'
+							                                                                                           expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount)
+							                                                                                           .toEqual(MINING_REWARD);
+							                                                                                          }
+							                                                      );
+							                                                   }
+							           );
 							  }
-							  
-							  
 		)
